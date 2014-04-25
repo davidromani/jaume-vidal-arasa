f8a99abe09513b9a1f30864ecebee82aeb0800b9
David Romaní <david@flux.cat>
2014-04-25 20:15:13 +0200

    fix this before

--- src/Acme/DemoBundle/DataFixtures/PHPCR/LoadDemoData.php
+++ src/Acme/DemoBundle/DataFixtures/PHPCR/LoadDemoData.php
@@ -20,25 +20,51 @@ class LoadDemoData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        // tweak homepage
-        $page = $manager->find(null, '/cms/simple');
-        $page->setBody('Hello');
-        $page->setDefault('_template', 'AcmeDemoBundle::home.html.twig');
+        /**
+         * contacte
+         */
+        /** @var Page $page */
+        $rootPage = $manager->find(null, '/cms/simple');
+        $page = new Page();
+        $page->setParentDocument($rootPage);
+        $page->setName('contacte');
+        $page->setTitle('contacte');
+        $page->setBody('contacte.');
+        $page->setDefault('_template', 'AcmeDemoBundle:page:index.html.twig');
+        $manager->persist($page);
+        // menu
+        $menuRoot = $manager->find(null, '/cms/simple');
+        $menuNode = new MenuNode('contacte');
+        $menuNode->setLabel('contacte');
+        $menuNode->setParent($menuRoot);
+        $menuNode->setContent($page);
+        $manager->persist($menuNode);
 
-        // add menu item for home
-        //$menuRoot = $manager->find(null, '/cms/simple');
-        //$homeMenuNode = new MenuNode('home');
-        //$homeMenuNode->setLabel('Home');
-        //$homeMenuNode->setParent($menuRoot);
-        //$homeMenuNode->setContent($page);
-
-        //$manager->persist($homeMenuNode);
+        /**
+         * treball recent
+         */
+        /** @var Page $page */
+        $rootPage = $manager->find(null, '/cms/simple');
+        $page = new Page();
+        $page->setParentDocument($rootPage);
+        $page->setName('treball-recent');
+        $page->setTitle('treball recent');
+        $page->setBody('trebal recent.');
+        $page->setDefault('_template', 'AcmeDemoBundle::works.html.twig');
+        $manager->persist($page);
+        // menu
+        $menuRoot = $manager->find(null, '/cms/simple');
+        $menuNode = new MenuNode('treball-recent');
+        $menuNode->setLabel('treball recent');
+        $menuNode->setParent($menuRoot);
+        $menuNode->setContent($page);
+        $manager->persist($menuNode);
 
         // load the yaml file in src/Acme/DemoBundle/Resources/data/pages.yml
-        Fixtures::load(array(__DIR__.'/../../Resources/data/pages.yml'), $manager);
+        //Fixtures::load(array(__DIR__.'/../../Resources/data/pages.yml'), $manager);
 
         // load the blocks
-        Fixtures::load(array(__DIR__.'/../../Resources/data/blocks.yml'), $manager);
+        //Fixtures::load(array(__DIR__.'/../../Resources/data/blocks.yml'), $manager);
 
         // save the changes
         $manager->flush();
