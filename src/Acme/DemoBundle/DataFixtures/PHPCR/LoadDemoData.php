be533b8d78cd004a59c69d0709912a8d6447cc86
David Romaní <david@flux.cat>
2014-04-25 23:34:30 +0200

    fix fixtures

--- src/Acme/DemoBundle/DataFixtures/PHPCR/LoadDemoData.php
+++ src/Acme/DemoBundle/DataFixtures/PHPCR/LoadDemoData.php
@@ -20,45 +20,66 @@ class LoadDemoData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        /**
-         * contacte
-         */
-        /** @var Page $page */
-        $rootPage = $manager->find(null, '/cms/simple');
-        $page = new Page();
-        $page->setParentDocument($rootPage);
-        $page->setName('contacte');
-        $page->setTitle('contacte');
-        $page->setBody('contacte.');
-        $page->setDefault('_template', 'AcmeDemoBundle:page:index.html.twig');
-        $manager->persist($page);
-        // menu
-        $menuRoot = $manager->find(null, '/cms/simple');
-        $menuNode = new MenuNode('contacte');
-        $menuNode->setLabel('contacte');
-        $menuNode->setParent($menuRoot);
-        $menuNode->setContent($page);
-        $manager->persist($menuNode);
+        $homepage = $manager->find(null, '/cms/simple');
+        $homepage->setBody('homepage');
+        $homepage->setDefault('_template', 'AcmeDemoBundle::home.html.twig');
+        $manager->persist($homepage);
 
-        /**
-         * treball recent
-         */
-        /** @var Page $page */
-        $rootPage = $manager->find(null, '/cms/simple');
         $page = new Page();
-        $page->setParentDocument($rootPage);
-        $page->setName('treball-recent');
-        $page->setTitle('treball recent');
-        $page->setBody('trebal recent.');
-        $page->setDefault('_template', 'AcmeDemoBundle::works.html.twig');
+        $page->setTitle('contacte');
+        $page->setLabel('contacte');
+        $page->setBody('contacte.');
+        $page->setDefault('_template', 'AcmeDemoBundle:Page:index.html.twig');
+        $page->setPosition($homepage, 'contacte');
         $manager->persist($page);
-        // menu
-        $menuRoot = $manager->find(null, '/cms/simple');
-        $menuNode = new MenuNode('treball-recent');
-        $menuNode->setLabel('treball recent');
-        $menuNode->setParent($menuRoot);
-        $menuNode->setContent($page);
-        $manager->persist($menuNode);
+
+        $page = new Page();
+        $page->setTitle('biografia');
+        $page->setLabel('biografia');
+        $page->setBody('biografia.');
+        $page->setDefault('_template', 'AcmeDemoBundle:Page:index.html.twig');
+        $page->setPosition($homepage, 'biografia');
+        $manager->persist($page);
+
+        $workpage = new Page();
+        $workpage->setTitle('treball recent');
+        $workpage->setLabel('treball recent');
+        $workpage->setBody('treball recent.');
+        $workpage->setDefault('_template', 'AcmeDemoBundle::works.html.twig');
+        $workpage->setPosition($homepage, 'treball-recent');
+        $manager->persist($workpage);
+
+        $page = new Page();
+        $page->setTitle('mondrian');
+        $page->setLabel('mondrian');
+        $page->setBody('mondrian.');
+        $page->setDefault('_template', 'AcmeDemoBundle::work.detail.html.twig');
+        $page->setPosition($workpage, 'mondrian');
+        $manager->persist($page);
+
+        $page = new Page();
+        $page->setTitle('castor');
+        $page->setLabel('castor');
+        $page->setBody('castor.');
+        $page->setDefault('_template', 'AcmeDemoBundle::work.detail.html.twig');
+        $page->setPosition($workpage, 'castor');
+        $manager->persist($page);
+
+        $page = new Page();
+        $page->setTitle('arxiu');
+        $page->setLabel('arxiu');
+        $page->setBody('arxiu.');
+        $page->setDefault('_template', 'AcmeDemoBundle:Page:index.html.twig');
+        $page->setPosition($homepage, 'arxiu');
+        $manager->persist($page);
+
+        $page = new Page();
+        $page->setTitle('publicacions');
+        $page->setLabel('publicacions');
+        $page->setBody('publicacions.');
+        $page->setDefault('_template', 'AcmeDemoBundle:Page:index.html.twig');
+        $page->setPosition($homepage, 'publicacions');
+        $manager->persist($page);
 
         // load the yaml file in src/Acme/DemoBundle/Resources/data/pages.yml
         //Fixtures::load(array(__DIR__.'/../../Resources/data/pages.yml'), $manager);
