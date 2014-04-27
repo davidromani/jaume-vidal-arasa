be378ccd41fcaba7b1ce946fe1e4214b96d7afed
David Romaní <david@flux.cat>
2014-04-27 09:52:16 +0200

    capifony deploy config refactor

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,19 +1,81 @@
-set :application, "set your application name here"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
-set :app_path,    "app"
+##########
+# Config #
+##########
+set :application,       "CMS Jaume Vidal Arasa"
+set :domain,            "s3.flux.cat"
+set :deploy_to,         "/home/flux/webapps/jaumevidalarasa"
+set :app_path,          "app"
+set :cache_dir,         "/cache"
+set :config_dir,        "/config"
+set :logs_dir,          "/logs"
+set :sessions_dir,      "/sessions"
+set :web_path,          "web"
+set :bundles_dir,       "/bundles"
+set :uploads_dir,       "/uploads"
+set :imagescache_dir,   "/imagescache"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
-set :scm,         :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+##################
+# SCM Repository #
+##################
+set :repository,        "git@bitbucket.org:dromani/cms-jaumevidalarasa.git"
+set :scm,               :git
+set :branch,            "master"
 
-set :model_manager, "doctrine"
-# Or: `propel`
+#######
+# ORM #
+#######
+set :model_manager,     "doctrine"              # Or "propel"
 
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
+#######
+# Web #
+#######
+role :web,  domain                              # Your HTTP server, Apache/etc
+role :app,  domain, :primary => true            # This may be the same as your `Web` server
 
-set  :keep_releases,  3
+##########
+# Server #
+##########
+set :user,              "flux"
+set :webserver_user,    "www-data"
+set :use_sudo,          false
+set :keep_releases,     3
+set :deploy_via,        :remote_cache           # Don't clone the whole repository into releases/ dir on every deploy
+set :use_composer,      true
+set :vendors_mode,      'install'
+set :update_vendors,    false                   # true = composer update | false = composer install
 
-# Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+###############
+# Shared dirs #
+###############
+set :shared_files,      [ app_path+config_dir+"/parameters.yml", web_path+"/.htaccess" ]
+set :shared_children,   [ app_path+logs_dir, app_path+sessions_dir, web_path+bundles_dir, web_path+uploads_dir, web_path+imagescache_dir ]
+
+###############
+# Permissions #
+###############
+set :writable_dirs,         [ app_path+cache_dir, app_path+logs_dir, app_path+sessions_dir, web_path+bundles_dir, web_path+uploads_dir, web_path+imagescache_dir ]
+set :permission_method,     :acl
+set :use_set_permissions,   true
+
+##############
+# Procedures #
+##############
+#namespace :jsroutes do
+#    desc "Exposing routes to javascripts"
+#    task :dump do
+#        run "cd #{release_path} && php app/console fos:js-routing:dump --env=prod"
+#    end
+#end
+
+############
+# Triggers #
+############
+#before "deploy:restart" do
+#    jsroutes.dump
+#end
+after "deploy", "deploy:cleanup"
+
+###########
+# Logging #
+###########
+logger.level = Logger::MAX_LEVEL               # Be more verbose by uncommenting this line
